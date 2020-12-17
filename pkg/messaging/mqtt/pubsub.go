4eb503ed4f7ffb450a9fbfcf079639c9458d7ec0
dusanb94 <dusan.borovcanin@mainflux.com>
2020-12-17 17:02:49 +0100

    Add MQTT client username
    Signed-off-by: dusanb94 <dusan.borovcanin@mainflux.com>

--- pkg/messaging/mqtt/pubsub.go
+++ pkg/messaging/mqtt/pubsub.go
@@ -12,13 +12,16 @@ import (
 
 const (
 	protocol = "mqtt"
+	username = "mainflux-mqtt"
 	qos      = 2
 )
 
 var errConnect = errors.New("failed to connect to MQTT broker")
 
 func newClient(address string, timeout time.Duration) (mqtt.Client, error) {
-	opts := mqtt.NewClientOptions().AddBroker(address)
+	opts := mqtt.NewClientOptions().
+		SetUsername(username).
+		AddBroker(address)
 	client := mqtt.NewClient(opts)
 	token := client.Connect()
 	if token.Error() != nil {
