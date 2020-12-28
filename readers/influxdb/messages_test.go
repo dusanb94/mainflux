14a46818f7a52345015351bbabe6b8ab58eee2d0
dusanb94 <dusan.borovcanin@mainflux.com>
2020-12-29 00:27:47 +0100

    Fix Influx Reader tests
    Signed-off-by: dusanb94 <dusan.borovcanin@mainflux.com>

--- readers/influxdb/messages_test.go
+++ readers/influxdb/messages_test.go
@@ -18,18 +18,21 @@ import (
 )
 
 const (
-	testDB   = "test"
-	chanID   = "1"
-	subtopic = "topic"
-	msgsNum  = 101
+	testDB     = "test"
+	chanID     = "1"
+	subtopic   = "topic"
+	msgsNum    = 100
+	fromToNum  = 4
+	msgsValNum = 20
+	limit      = 10
 )
 
 var (
-	v       float64 = 5
-	stringV         = "value"
-	boolV           = true
-	dataV           = "base64"
-	sum     float64 = 42
+	v   float64 = 5
+	vs          = "value"
+	vb          = true
+	vd          = "dataValue"
+	sum float64 = 42
 )
 
 var (
@@ -58,31 +61,37 @@ func TestReadAll(t *testing.T) {
 	writer := writer.New(client, testDB)
 
 	messages := []senml.Message{}
-	subtopicMsgs := []senml.Message{}
+	valSubtopicMsgs := []senml.Message{}
+	boolMsgs := []senml.Message{}
+	stringMsgs := []senml.Message{}
+	dataMsgs := []senml.Message{}
+
 	now := time.Now().UnixNano()
 	for i := 0; i < msgsNum; i++ {
 		// Mix possible values as well as value sum.
-		count := i % valueFields
 		msg := m
+		msg.Time = float64(now)/1e9 - float64(i)
+
+		count := i % valueFields
 		switch count {
 		case 0:
 			msg.Subtopic = subtopic
 			msg.Value = &v
+			valSubtopicMsgs = append(valSubtopicMsgs, msg)
 		case 1:
-			msg.BoolValue = &boolV
+			msg.BoolValue = &vb
+			boolMsgs = append(boolMsgs, msg)
 		case 2:
-			msg.StringValue = &stringV
+			msg.StringValue = &vs
+			stringMsgs = append(stringMsgs, msg)
 		case 3:
-			msg.DataValue = &dataV
+			msg.DataValue = &vd
+			dataMsgs = append(dataMsgs, msg)
 		case 4:
 			msg.Sum = &sum
 		}
 
-		msg.Time = float64(now)/float64(1e9) - float64(i)
 		messages = append(messages, msg)
-		if count == 0 {
-			subtopicMsgs = append(subtopicMsgs, msg)
-		}
 	}
 
 	err := writer.Save(messages)
@@ -101,34 +110,34 @@ func TestReadAll(t *testing.T) {
 		"read message page for existing channel": {
 			chanID: chanID,
 			offset: 0,
-			limit:  10,
+			limit:  limit,
 			page: readers.MessagesPage{
 				Total:    msgsNum,
 				Offset:   0,
-				Limit:    10,
-				Messages: fromSenml(messages[0:10]),
+				Limit:    limit,
+				Messages: fromSenml(messages[0:limit]),
 			},
 		},
 		"read message page for non-existent channel": {
 			chanID: "2",
 			offset: 0,
-			limit:  10,
+			limit:  limit,
 			page: readers.MessagesPage{
 				Total:    0,
 				Offset:   0,
-				Limit:    10,
+				Limit:    limit,
 				Messages: []interface{}{},
 			},
 		},
 		"read message last page": {
 			chanID: chanID,
 			offset: 95,
-			limit:  10,
+			limit:  limit,
 			page: readers.MessagesPage{
 				Total:    msgsNum,
 				Offset:   95,
-				Limit:    10,
-				Messages: fromSenml(messages[95:101]),
+				Limit:    limit,
+				Messages: fromSenml(messages[95:msgsNum]),
 			},
 		},
 		"read message with non-existent subtopic": {
@@ -146,13 +155,76 @@ func TestReadAll(t *testing.T) {
 		"read message with subtopic": {
 			chanID: chanID,
 			offset: 0,
-			limit:  10,
+			limit:  limit,
 			query:  map[string]string{"subtopic": subtopic},
 			page: readers.MessagesPage{
-				Total:    uint64(len(subtopicMsgs)),
+				Total:    uint64(len(valSubtopicMsgs)),
 				Offset:   0,
-				Limit:    10,
-				Messages: fromSenml(subtopicMsgs[0:10]),
+				Limit:    limit,
+				Messages: fromSenml(valSubtopicMsgs[0:limit]),
+			},
+		},
+		"read message with value": {
+			chanID: chanID,
+			offset: 0,
+			limit:  limit,
+			query:  map[string]string{"v": fmt.Sprintf("%f", v)},
+			page: readers.MessagesPage{
+				Total:    msgsValNum,
+				Offset:   0,
+				Limit:    limit,
+				Messages: fromSenml(valSubtopicMsgs[0:limit]),
+			},
+		},
+		"read message with boolean value": {
+			chanID: chanID,
+			offset: 0,
+			limit:  limit,
+			query:  map[string]string{"vb": fmt.Sprintf("%t", vb)},
+			page: readers.MessagesPage{
+				Total:    msgsValNum,
+				Offset:   0,
+				Limit:    limit,
+				Messages: fromSenml(boolMsgs[0:limit]),
+			},
+		},
+		"read message with string value": {
+			chanID: chanID,
+			offset: 0,
+			limit:  limit,
+			query:  map[string]string{"vs": vs},
+			page: readers.MessagesPage{
+				Total:    msgsValNum,
+				Offset:   0,
+				Limit:    limit,
+				Messages: fromSenml(stringMsgs[0:limit]),
+			},
+		},
+		"read message with data value": {
+			chanID: chanID,
+			offset: 0,
+			limit:  limit,
+			query:  map[string]string{"vd": vd},
+			page: readers.MessagesPage{
+				Total:    msgsValNum,
+				Offset:   0,
+				Limit:    limit,
+				Messages: fromSenml(dataMsgs[0:limit]),
+			},
+		},
+		"read message with from/to": {
+			chanID: chanID,
+			offset: 0,
+			limit:  limit,
+			query: map[string]string{
+				"from": fmt.Sprintf("%f", messages[fromToNum].Time),
+				"to":   fmt.Sprintf("%f", messages[0].Time),
+			},
+			page: readers.MessagesPage{
+				Total:    fromToNum,
+				Offset:   0,
+				Limit:    limit,
+				Messages: fromSenml(messages[1:5]),
 			},
 		},
 	}
@@ -160,7 +232,7 @@ func TestReadAll(t *testing.T) {
 	for desc, tc := range cases {
 		result, err := reader.ReadAll(tc.chanID, tc.offset, tc.limit, tc.query)
 		assert.Nil(t, err, fmt.Sprintf("%s: expected no error got %s", desc, err))
-		assert.ElementsMatch(t, tc.page.Messages, result.Messages, fmt.Sprintf("%s: expected: %v \n-------------\n got: %v", desc, tc.page.Messages, result.Messages))
+		assert.ElementsMatch(t, tc.page.Messages, result.Messages, fmt.Sprintf("%s: expected: %v, got: %v", desc, tc.page.Messages, result.Messages))
 
 		assert.Equal(t, tc.page.Total, result.Total, fmt.Sprintf("%s: expected %d got %d", desc, tc.page.Total, result.Total))
 	}
