ee7671f27fceb8ccc51c7ac5e4bd6a8636facb2a
dusanb94 <dusan.borovcanin@mainflux.com>
2021-02-08 18:34:17 +0100

    Add SMTP notify logic
    Signed-off-by: dusanb94 <dusan.borovcanin@mainflux.com>

--- consumers/notifiers/smtp/notifier.go
+++ consumers/notifiers/smtp/notifier.go
@@ -9,6 +9,8 @@ import (
 	notifiers "github.com/mainflux/mainflux/consumers/notifiers"
 	"github.com/mainflux/mainflux/internal/email"
 	"github.com/mainflux/mainflux/pkg/messaging"
+	"github.com/mainflux/mainflux/pkg/transformers"
+	"github.com/mainflux/mainflux/pkg/transformers/json"
 )
 
 const (
@@ -18,16 +20,24 @@ const (
 
 var _ notifiers.Notifier = (*notifier)(nil)
 
+var fields = [...]string{"s_leakage", "s_blocked", "s_magnet", "s_blowout", "ALM", "magnet"}
+
 type notifier struct {
 	agent *email.Agent
+	tr    transformers.Transformer
 }
 
 // New instantiates SMTP message notifier.
 func New(agent *email.Agent) notifiers.Notifier {
-	return &notifier{agent: agent}
+	return &notifier{agent: agent, tr: json.New()}
 }
 
 func (n *notifier) Notify(from string, to []string, msg messaging.Message) error {
+	m, err := json.New().Transform(msg)
+	if err != nil {
+		return err
+	}
+
 	subject := fmt.Sprintf(`Notification for Channel %s`, msg.Channel)
 	if msg.Subtopic != "" {
 		subject = fmt.Sprintf("%s and subtopic %s", subject, msg.Subtopic)
@@ -35,6 +45,26 @@ func (n *notifier) Notify(from string, to []string, msg messaging.Message) error
 
 	values := string(msg.Payload)
 	content := fmt.Sprintf(contentTemplate, msg.Publisher, msg.Protocol, values)
+	switch t := m.(type) {
+	case map[string]interface{}:
+		for _, k := range fields {
+			if v, ok := t[k]; v != nil && ok {
+				if val, ok := v.(int); ok && val != 0 {
+					return n.agent.Send(to, from, subject, "", content, footer)
+				}
+			}
+		}
+	case []map[string]interface{}:
+		for _, v := range t {
+			for _, k := range fields {
+				if v, ok := v[k]; v != nil && ok {
+					if val, ok := v.(int); ok && val != 0 {
+						return n.agent.Send(to, from, subject, "", content, footer)
+					}
+				}
+			}
+		}
+	}
 
-	return n.agent.Send(to, from, subject, "", content, footer)
+	return nil
 }
